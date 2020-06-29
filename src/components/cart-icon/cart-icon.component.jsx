482965dc18dfa1caf1bae17bd4368a232deb42d6
Wasif Baliyan <w.baliyan@gmail.com>
2020-06-29 09:16:52 +0530

    add reselect, update cart items with memoized cartitems with reselect

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,6 +5,7 @@ import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
 import "./cart-icon.styles.scss";
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
+import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
 
 const CartIcon = ({ hidden, toggleCartHidden, itemCount }) => (
   <div className="cart-icon">
@@ -13,11 +14,8 @@ const CartIcon = ({ hidden, toggleCartHidden, itemCount }) => (
   </div>
 );
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  itemCount: cartItems.reduce(
-    (accumulaterQuantity, cartItem) => accumulaterQuantity + cartItem.quantity,
-    0
-  ),
+const mapStateToProps = (state) => ({
+  itemCount: selectCartItemsCount(state),
 });
 
 const mapDispatchToProps = (dispatch) => {
