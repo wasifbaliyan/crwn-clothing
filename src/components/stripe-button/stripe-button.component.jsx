17940ce3c3dc403912aaf7b0575d3c99fee65992
Wasif Baliyan <w.baliyan@gmail.com>
2020-09-11 19:48:23 +0530

    integrate stripe for payment

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51HQ9ApHGNxJzLUgU3cob3h58HRyVckAZaxeSzzbYZbfXXLGHwktVW7bRqoAig3kqVqgt9RycC1vnOEbqh0lnBZvP00AZG2OegN";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing LLP"
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
