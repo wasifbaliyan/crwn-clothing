0fb3508664c44b8bb3f28afca45d18ee6c6a7985
Wasif Baliyan <w.baliyan@gmail.com>
2020-09-27 17:19:32 +0530

    added withSpinner HOC to show spinner before data fetched from API

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -17,13 +17,14 @@ export const selectCollections = createSelector(
 
 export const selectCollectionsForPreview = createSelector(
   [selectCollections],
-  (collections) => Object.keys(collections).map((key) => collections[key])
+  (collections) =>
+    collections ? Object.keys(collections).map((key) => collections[key]) : []
 );
 
 export const selectCollection = (collectionUrlParam) =>
   createSelector(
     [selectCollections],
-    (collections) => collections[collectionUrlParam]
+    (collections) => (collections ? collections[collectionUrlParam] : null)
     // collections.find(
     //   (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
     // )
